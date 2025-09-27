bb4c72101299f0d7927fed62093a08291b21f768
thisiskannamma <MY_NAME@example.com>
2025-09-27 16:20:19 +0530

    updated

--- portfolio-project/src/Components/About.jsx
+++ portfolio-project/src/Components/About.jsx
@@ -9,7 +9,7 @@ function About() {
         </div>
         <div className='about-text'>
           <h2>About <span>Me</span></h2>
-          <h4>Full Stack Developer!</h4>
+          <h4>Civil Engineer!</h4>
           <p>Hello! I'm Elizabeth Kannamma, a Civil Engineer passionate about building design, structural planning, and construction management. Skilled in AutoCAD, Revit, and STAAD.Pro, I create precise building plans and structural designs. I’m eager to contribute to projects that combine innovation, quality, and safety.</p>
           <a href="" className='btn-box'>More About me</a>
         </div>
